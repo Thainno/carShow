6e6391e4ea5fdd40076c0c38e347554b7707274c
Tainu <thainnosv@gmail.com>
2024-10-13 18:00:13 -0300

    position cars

--- src/app/cars/supra/page.js
+++ src/app/cars/supra/page.js
@@ -4,19 +4,19 @@ export default function Supra() {
   return (
     <>
       <section className="w-screem h-screen">
-        <div className="flex flex-row justify-between px-9 pt-28 h-5/6">
-          <div className="functions-cars bg-slate-400 w-1/5">FUNCTIONS</div>
-          <div className="show-car bg-slate-600 w-3/5 flex items-center">
+        <div className="flex flex-row justify-between px-9 pt-28 h-5/6 relative">
+          <div className="functions-cars bg-slate-400 w-72">FUNCTIONS</div>
+          <picture className="absolute w-[1294px] h-[528px] flex justify-center items-end">
             <Image
               src="/images/cars/supra.png"
               alt="Supra Mk4"
-              width={3440}
+              width={900}
               height={0}
             />
-          </div>
-          <div className="about-car bg-slate-400 w-1/5">ABOUT THIS CAR</div>
+          </picture>
+          <div className="about-car bg-slate-400 w-72">ABOUT THIS CAR</div>
         </div>
-        <div className="flex flex-row items-center justify-between px-9 pt-4 h-1/6">
+        <div className="flex flex-row items-center justify-between px-9 h-1/6">
           <div>OPTIONS</div>
           <div className="flex flex-row items-center justify-center gap-4">
             <span className="text-cyan-50">{"<"}</span>
