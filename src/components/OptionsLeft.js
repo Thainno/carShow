8fb86b4cee588ae7aeade3432e08f9a0856b50c9
Tainu <thainnosv@gmail.com>
2024-10-14 21:56:31 -0300

    start card left menu

--- src/components/OptionsLeft.js
+++ src/components/OptionsLeft.js
@@ -1,3 +1,11 @@
 export default function OptionsLeft() {
-  return <div>OPTIONS</div>;
+  return (
+    <>
+      <div>OPTION 01</div>
+      <div>OPTION 02</div>
+      <div>OPTION 03</div>
+      <div>OPTION 04</div>
+      <div>OPTION 05</div>
+    </>
+  );
 }
