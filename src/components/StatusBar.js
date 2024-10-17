7b5060d928ace74570caad69a3f8a714aa64d787
Tainu <thainnosv@gmail.com>
2024-10-17 10:14:15 -0300

    add statusBar info

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,8 +1,11 @@
 export default function StatusBar() {
   return (
     <>
-      <div className="bg-slate-500 h-4 w-[272px] absolute"></div>
-      <div className="bg-slate-700 h-4 w-16 absolute"></div>
+      <div className="bg-slate-500 h-6 w-[272px] absolute"></div>
+      <div className="bg-slate-700 h-6 w-16 absolute"></div>
+      <div className="p-auto bg-slate-700 h-6 w-12 absolute right-0 text-white">
+        400cv
+      </div>
     </>
   );
 }
