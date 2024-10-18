6efa6ed45f92dcf0265d2cb061e0b12a255db70f
Tainu <thainnosv@gmail.com>
2024-10-18 00:21:43 -0300

    start CarRoll

--- src/components/cars/CarRoll.js
+++ src/components/cars/CarRoll.js
@@ -1,12 +1,25 @@
 export default function CarRoll() {
   return (
-    <div className="flex flex-row items-center justify-center gap-4">
-      <span className="text-cyan-50">{"<"}</span>
-      <div className="w-16 h-16 bg-white"></div>
-      <div className="w-16 h-16 bg-white"></div>
-      <div className="w-16 h-16 bg-white"></div>
-      <div className="w-16 h-16 bg-white"></div>
-      <span className="text-cyan-50">{">"}</span>
+    <div className="flex flex-row items-center justify-center relative w-64 h-16">
+      <span className="text-cyan-50 absolute -left-2">{"<"}</span>
+      <img
+        src="/images/cars/gtr-r34.png"
+        className="w-16 h-8 absolute left-4 top-1.5"
+      ></img>
+      <img
+        src="/images/cars/gtr-r34.png"
+        className="w-20 h-10 absolute left-8 top-2"
+      ></img>
+      <img src="/images/cars/gtr-r34.png" className="w-22 h-12 absolute"></img>
+      <img
+        src="/images/cars/gtr-r34.png"
+        className="w-20 h-10 absolute right-8 top-2"
+      ></img>
+      <img
+        src="/images/cars/gtr-r34.png"
+        className="w-16 h-8 absolute right-4 top-1.5"
+      ></img>
+      <span className="text-cyan-50 absolute -right-2">{">"}</span>
     </div>
   );
 }
